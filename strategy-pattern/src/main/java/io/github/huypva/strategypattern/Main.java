188e35171d0e629fc286ca56a04359acbcb81369
huypva <huypva@vng.com.vn>
2023-05-18 10:28:29 +0700

    Rename Client class to Main class

--- strategy-pattern/src/main/java/io/github/huypva/strategypattern/Main.java
+++ strategy-pattern/src/main/java/io/github/huypva/strategypattern/Main.java
@@ -3,7 +3,7 @@ package io.github.huypva.strategypattern;
 /**
  * @author huypva
  */
-public class Client {
+public class Main {
 
   public static void main(String[] args) {
     Context context = new Context(new ConcreteStrategy1());
